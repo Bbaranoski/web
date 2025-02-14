d8932ec06f25ab7f7f4eec223300af43cae0fbd0
Bbaranoski <breno.b.o.baranoski@gmail.com>
2025-02-14 14:04:37 -0300

    add a style to the input

--- src/cliente.tsx
+++ src/cliente.tsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import Consulta from "./consulta";
 import { useContext } from "react";
 import { StyleContext } from "./App";
-import { Box, Color, Tabs, VStack, HStack, Fieldset, Field, Input } from "@chakra-ui/react";
+import { Box,
+        Color,
+        Tabs,
+        VStack,
+        HStack,
+        Fieldset,
+        Field,
+        Input,
+        defineStyle } from "@chakra-ui/react";
 import { GoPerson } from "react-icons/go";
 import { IoIosAirplane } from "react-icons/io";
 //interface de pessoa
@@ -45,6 +53,34 @@ const Cadastro: React.FC = () => {
             font: string,
             titulo: string,
             corBorda: string }
+
+            console.log(style)
+
+    const floatingStyles = defineStyle({
+    color: style.corTexto,
+    pos: "absolute",
+    bg: "bg",
+    bgColor: style.cor,
+    px: "0.5",
+    top: "-3",
+    insetStart: "2",
+    fontWeight: "normal",
+    pointerEvents: "none",
+    transition: "position",
+    _peerPlaceholderShown: {
+      color: style.corBorda,
+      bgColor: style.cor,
+      top: "2.5",
+      insetStart: "3",
+    },
+    _peerFocusVisible: {
+      color: style.corTexto,
+      bgColor: style.cor,
+      top: "-3",
+      insetStart: "2",
+    },
+    })
+    
     return (
         <Tabs.Root defaultValue='infoPrincipal'>
             <Tabs.List >
@@ -68,12 +104,18 @@ const Cadastro: React.FC = () => {
                 >
                     <Fieldset.Root>
                         <HStack width={'100%'}
+                        padding={'1%'}
                         >
+                            <Field.Root>
+                                <Box pos="relative" w="full">
+                                    <Input className="peer" color= {style.corTexto} placeholder="" focusRingColor={style.corTexto}/>
+                                    <Field.Label css={floatingStyles}>Nome</Field.Label>
+                                </Box>
+                            </Field.Root>
                             <Field.Root>
                                 <Field.Label>Nome</Field.Label>
                                 <Input placeholder="Nome" />
                             </Field.Root>
-                            
                         </HStack>
                     </Fieldset.Root>
                 </VStack>
